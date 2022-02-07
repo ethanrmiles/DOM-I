8c9152580e288ce14a47d28401949d5bae97bee1
Ethan Miles <ethanmilescs@gmail.com>
2022-02-07 14:47:06 -0600

    completed cta and nav

--- src/index.js
+++ src/index.js
@@ -72,4 +72,22 @@ topContent.children[1].children[1].textContent = siteContent["main-content"]['ab
 
 //Bottom Content 
 const bottomContent = document.querySelector('.bottom-content');
-console.log(bottomContent)
+const h4sBottom = bottomContent.querySelectorAll('h4');
+h4sBottom[0].textContent = siteContent['main-content']['services-h4'];
+h4sBottom[1].textContent = siteContent['main-content']['product-h4'];
+h4sBottom[2].textContent = siteContent['main-content']['vision-h4'];
+const pBottom = bottomContent.querySelectorAll('p')
+pBottom[0].textContent = siteContent['main-content']['features-content'];
+pBottom[1].textContent = siteContent['main-content']['about-content'];
+pBottom[2].textContent = siteContent['main-content']['vision-content'];
+
+//cta 
+document.querySelector('.cta .cta-text h1').textContent = siteContent.cta.h1
+document.querySelector('.cta .cta-text button').textContent = siteContent.cta.button
+
+//nav links
+const navLinks = document.querySelectorAll('header nav a ')
+const navLinksText = Object.values(siteContent.nav)
+navLinks.forEach((link, idx) => {
+  link.textContent = navLinksText[idx]
+})
